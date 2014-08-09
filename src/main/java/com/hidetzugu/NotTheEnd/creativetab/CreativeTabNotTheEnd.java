8c3efce9fca0c2be103f74734932e9769c532ab3
hidetzugu <hidetzugu@gmail.com>
2014-08-09 13:39:00 +0100

    added a NotTheEnd creative tab

--- src/main/java/com/hidetzugu/NotTheEnd/creativetab/CreativeTabNotTheEnd.java
+++ src/main/java/com/hidetzugu/NotTheEnd/creativetab/CreativeTabNotTheEnd.java
@@ -7,15 +7,10 @@ import net.minecraft.item.Item;
 
 public class CreativeTabNotTheEnd {
 
-    public static final CreativeTabs NotTheEnd_Tab =new CreativeTabs(Reference.MOD_ID){
+    public static final CreativeTabs NotTheEnd_Tab =new CreativeTabs(Reference.MOD_ID.toLowerCase()){
         @Override
         public Item getTabIconItem() {
             return Item.getItemFromBlock(Blocks.dragon_egg);
-        }
-
-        @Override
-        public String getTranslatedTabLabel(){
-            return "NotTheEnd";
         }
     };
 
