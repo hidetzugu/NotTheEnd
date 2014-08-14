57c812eeb3a3dfe30adf1b361fac4bb4b264b810
hidetzugu <hidetzugu@gmail.com>
2014-08-14 23:11:17 +0100

    finished registering ore smelting

--- src/main/java/com/hidetzugu/NotTheEnd/init/OreRegistration.java
+++ src/main/java/com/hidetzugu/NotTheEnd/init/OreRegistration.java
@@ -12,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraftforge.oredict.OreDictionary;
 import scala.swing.ListView;
+import scala.tools.nsc.typechecker.Typers;
 
 import javax.swing.plaf.basic.BasicComboBoxUI;
 
@@ -37,9 +38,20 @@ public class OreRegistration {
     }
 
     public static void RegisterSmelting(){
-
+        //metals
         GameRegistry.addSmelting(new ItemStack(ModBlocks.Ender_iron_ore),new ItemStack(Items.iron_ingot,1),1.0f);
         GameRegistry.addSmelting(ModBlocks.Ender_gold_ore,new ItemStack(Items.gold_ingot,1),1.0f);
+        //minerals
+        GameRegistry.addSmelting(ModBlocks.Ender_redstone_ore,new ItemStack(Items.redstone,3),1.0f);
+        GameRegistry.addSmelting(ModBlocks.Ender_redstone_cluster,new ItemStack(Items.redstone,5),1.0f);
+
+        GameRegistry.addSmelting(ModBlocks.Ender_diamond_ore,new ItemStack(Items.diamond,1),1.0f);
+        GameRegistry.addSmelting(ModBlocks.Ender_diamond_cluster,new ItemStack(Items.diamond,2),1.0f);
+        GameRegistry.addSmelting(ModBlocks.Ender_emerald_ore,new ItemStack(Items.emerald,1),1.0f);
+        GameRegistry.addSmelting(ModBlocks.Ender_emerald_cluster,new ItemStack(Items.emerald,2),1.0f);
+            //lapiz (the item) does NOT has a specific item object; instead its a dye object with meta-data 4
+        GameRegistry.addSmelting(ModBlocks.Ender_lapis_ore,new ItemStack(Items.dye,3,4),1.0f);
+
 
     }
 }
